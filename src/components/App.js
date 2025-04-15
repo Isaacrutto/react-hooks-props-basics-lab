218002e1c076cf64a79add149e9040bf0fb3e944
Isaac Rutto <isaacrutto3608@gmail.com>
2025-04-15 10:44:00 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -3,16 +3,17 @@ import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
 
-// pass this data down as props to the child component(s) that need it!
-import user from "../data/user";
-console.log(user);
+// Pass this data down as props to the child component(s) that need it!
+import { username, city, bio, github, linkedin } from "../data/user"; // Assuming user is exported as named exports
+
+console.log({ username, city, bio, github, linkedin });
 
 function App() {
   return (
     <div>
       <NavBar />
-      <Home />
-      <About />
+      <Home name={username} city={city} />
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
   );
 }
